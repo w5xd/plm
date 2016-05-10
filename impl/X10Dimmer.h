7f68cb2aec4df7c3de48046baf14f1fb7bd4f987
Wayne Wright <w5xd@alum.mit.edu>
2016-05-10 02:34:13 +0000

    Update X10Dimmer.h

--- impl/X10Dimmer.h
+++ impl/X10Dimmer.h
@@ -6,30 +6,25 @@ namespace w5xdInsteon {
 	class PlmMonitor;
     class X10Dimmer 
     {
-        /* This device is a single dimmer. It has one button, which is numbered 1
-        ** The same class may be used to control a relay. Any nonzero setting is on and
-        ** reports back as value 255. Relaylinc's also are controlled fine by this class,
-        ** including the one with a sense wire. It acts pretty much like a wall switch
-        ** from a software perspective.
+        /* This device is a single "dimmer" but currently only ON and OFF are supported.
         */
     public:
         X10Dimmer(PlmMonitor *plm, char houseCode, unsigned char unit)
 			: m_houseCode(houseCode)
 			, m_unit(unit)
 			, m_plm(plm)
-		{
-		}
-		bool operator < (const X10Dimmer &other) const
-		{
-			if (m_houseCode == other.m_houseCode)
-				return m_unit < other.m_unit;
-			return m_houseCode < other.m_houseCode;
-		}
+	{}
+	bool operator < (const X10Dimmer &other) const
+	{
+		if (m_houseCode == other.m_houseCode)
+			return m_unit < other.m_unit;
+		return m_houseCode < other.m_houseCode;
+	}
         int setDimmerValue(unsigned char) const;
     protected:
-		const char m_houseCode;
-		const unsigned char m_unit;
-		PlmMonitor  *m_plm; // non ref-counted
+	const char m_houseCode;
+	const unsigned char m_unit;
+	PlmMonitor  *m_plm; // non ref-counted
     };
 
 }
