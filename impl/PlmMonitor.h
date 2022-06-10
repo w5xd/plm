658f1c099ff823b7428750b8977760e5e9402c2a
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2022-06-10 21:55:22 +0000

    process nak message

--- impl/PlmMonitor.h
+++ impl/PlmMonitor.h
@@ -18,24 +18,24 @@
 
 namespace w5xdInsteon {
     class Dimmer;
-	class X10Dimmer;
-	class X10DimmerCollection;
+    class X10Dimmer;
+    class X10DimmerCollection;
     class Keypad;
     class Fanlinc;
     class InsteonDevice;
     class PlmMonitorIO;
     class InsteonLinkEntry;
 
-    extern void bufferToStream(std::ostream &st, const unsigned char *v, int s);
+    extern void bufferToStream(std::ostream& st, const unsigned char* v, int s);
 
     // class for holding pointers to InsteonDevices sorted by their Device address.
     class InsteonDevicePtr
     {
     public:
-        InsteonDevicePtr(InsteonDevice *p) : m_p(p){} // takes ownership of p
-        InsteonDevicePtr(std::shared_ptr<InsteonDevice> p) : m_p(p){}
+        InsteonDevicePtr(InsteonDevice* p) : m_p(p) {} // takes ownership of p
+        InsteonDevicePtr(std::shared_ptr<InsteonDevice> p) : m_p(p) {}
         InsteonDevicePtr(const unsigned char addr[3]);
-        bool operator < (const InsteonDevicePtr &other) const ;
+        bool operator < (const InsteonDevicePtr& other) const;
     public:
         std::shared_ptr<InsteonDevice>    m_p;
     };
@@ -44,28 +44,32 @@ namespace w5xdInsteon {
     class InsteonCommand
     {
     public:
-        typedef std::function<void (InsteonCommand *)> CompletionCb_t;
-        InsteonCommand(unsigned rlen, int retry=1, CompletionCb_t fcn=0) :
-          m_responseLength(rlen), m_answerState(0),m_timesSent(0), m_retry(retry), m_whenDone(fcn)
-          {}
+        typedef std::function<void(InsteonCommand*)> CompletionCb_t;
+        typedef std::function<void(InsteonCommand*, const std::vector<unsigned char>&)> OnNakCb_t;
+        InsteonCommand(unsigned rlen, int retry = 1, CompletionCb_t fcn = CompletionCb_t(), OnNakCb_t OnNak = OnNakCb_t()) :
+            m_responseLength(rlen), m_answerState(0), m_timesSent(0), m_retry(retry), m_whenDone(fcn)
+            , m_timesNak(4), m_whenNAK(OnNak)
+        {}
         std::vector<unsigned char>   m_command;
-        const unsigned m_responseLength;
+        unsigned m_responseLength;
         std::shared_ptr<std::vector<unsigned char> >  m_answer;
         int m_answerState;   // 0 means waiting. negative is error code. positive is finished OK
         int m_timesSent;
         int m_retry;
+        int m_timesNak;
         int m_globalId;
         CompletionCb_t m_whenDone;
+        OnNakCb_t m_whenNAK;
     };
 
     class PlmMonitor
     {
     public:
-        PlmMonitor(const char *commPortName, const char *logFileName=0);
+        PlmMonitor(const char* commPortName, const char* logFileName = 0);
         ~PlmMonitor();
-        int which() const { return m_which;}
+        int which() const { return m_which; }
         int init();
-        const std::string &commPortName() const;
+        const std::string& commPortName() const;
 
         int shutdownModem();
         int startLinking(int, bool);
@@ -75,56 +79,63 @@ namespace w5xdInsteon {
         int clearModemLinkData();
         int setAllDevices(unsigned char grp, unsigned char v);
         int nextUnusedControlGroup()const;
-        const char * printModemLinkTable() ;
-	    int printLogString(const char *);
+        const char* printModemLinkTable();
+        int printLogString(const char*);
 
         template <class T>
-        T *getDeviceAccess(const unsigned char addr[3]);
+        T* getDeviceAccess(const unsigned char addr[3]);
         template <class T>
-        T *getDeviceAccess(const char *addr);
+        T* getDeviceAccess(const char* addr);
 
         // C callable dimmer--access by string name
-        Dimmer *getDimmerAccess(const char *addr)
-        {return getDeviceAccess<Dimmer>(addr);}
-        Keypad *getKeypadAccess(const char *addr)
-        {return getDeviceAccess<Keypad>(addr);}
-        Fanlinc *getFanlincAccess(const char *addr)
-        {return getDeviceAccess<Fanlinc>(addr);}
-		X10Dimmer *getX10DimmerAccess(char houseCode, unsigned char unit);
+        Dimmer* getDimmerAccess(const char* addr)
+        {
+            return getDeviceAccess<Dimmer>(addr);
+        }
+        Keypad* getKeypadAccess(const char* addr)
+        {
+            return getDeviceAccess<Keypad>(addr);
+        }
+        Fanlinc* getFanlincAccess(const char* addr)
+        {
+            return getDeviceAccess<Fanlinc>(addr);
+        }
+        X10Dimmer* getX10DimmerAccess(char houseCode, unsigned char unit);
 
-		int sendX10Command(char houseCode, unsigned short unitMask, enum X10Commands_t command);
+        int sendX10Command(char houseCode, unsigned short unitMask, enum X10Commands_t command);
 
-        std::shared_ptr<InsteonCommand> 
-            queueCommand(const unsigned char *v, unsigned s, unsigned resLen, bool retry = true, InsteonCommand::CompletionCb_t fcn=0);
-        std::shared_ptr<InsteonCommand> 
-            sendCommandAndWait(const unsigned char *v, unsigned s, unsigned resLen, bool retry = true);
+        std::shared_ptr<InsteonCommand>
+            queueCommand(const unsigned char* v, unsigned s, unsigned resLen, bool retry = true, InsteonCommand::CompletionCb_t fcn = InsteonCommand::CompletionCb_t(),
+                InsteonCommand::OnNakCb_t onNak = InsteonCommand::OnNakCb_t());
+        std::shared_ptr<InsteonCommand>
+            sendCommandAndWait(const unsigned char* v, unsigned s, unsigned resLen, bool retry = true);
 
-        int createLink(InsteonDevice *who, bool amControl,
+        int createLink(InsteonDevice* who, bool amControl,
             unsigned char grp, unsigned char flag, unsigned char ls1, unsigned char ls2, unsigned char ls3);
-        int removeLink(InsteonDevice *who, bool amControl, unsigned char grp);
+        int removeLink(InsteonDevice* who, bool amControl, unsigned char grp);
 
-        int setIfLinked(InsteonDevice *d, unsigned char v);
-        int setFastIfLinked(InsteonDevice *d, int v);
+        int setIfLinked(InsteonDevice* d, unsigned char v);
+        int setFastIfLinked(InsteonDevice* d, int v);
 
         int deleteGroupLinks(unsigned char group);
 
-        int monitor(int waitSeconds, InsteonDevice *&dimmer, 
-            unsigned char &group, unsigned char &cmd1, unsigned char &cmd2,
-            unsigned char &ls1, unsigned char &ls2, unsigned char &ls3);
+        int monitor(int waitSeconds, InsteonDevice*& dimmer,
+            unsigned char& group, unsigned char& cmd1, unsigned char& cmd2,
+            unsigned char& ls1, unsigned char& ls2, unsigned char& ls3);
 
         void queueNotifications(bool v);
 
-        enum {MESSAGE_QUIET, MESSAGE_ON, MESSAGE_MOST_IO, MESSAGE_EVERY_IO};
+        enum { MESSAGE_QUIET, MESSAGE_ON, MESSAGE_MOST_IO, MESSAGE_EVERY_IO };
         int m_verbosity;
-        int setErrorLevel(int v) { int ret = m_verbosity; m_verbosity = v; return ret;}
-        int getErrorLevel() const { return m_verbosity;}
+        int setErrorLevel(int v) { int ret = m_verbosity; m_verbosity = v; return ret; }
+        int getErrorLevel() const { return m_verbosity; }
         int setCommandDelay(int delayMsec);
-        const unsigned char *insteonID()const{return m_IMInsteonId;}
-        std::ostream &cerr(){return m_errorFile.is_open() ? m_errorFile : std::cerr;}
+        const unsigned char* insteonID()const { return m_IMInsteonId; }
+        std::ostream& cerr() { return m_errorFile.is_open() ? m_errorFile : std::cerr; }
         static const unsigned char SET_ACQ_MSG_BYTE;
         static const unsigned char SET_ACQ_MSG_2BYTE;
         static const unsigned char SET_NAQ_MSG_BYTE;
-   protected:
+    protected:
         typedef std::set<InsteonDevicePtr> InsteonDeviceSet_t;
         struct NotificationEntry {
             std::shared_ptr<InsteonDevice> m_device;
@@ -135,9 +146,10 @@ namespace w5xdInsteon {
             unsigned char ls2;
             unsigned char ls3;
             std::chrono::steady_clock::time_point    m_received;
-            NotificationEntry() : group(0), cmd1(0), cmd2(0), ls1(0), ls2(0), ls3(0){}
-            bool operator == (const NotificationEntry &other)const;
+            NotificationEntry() : group(0), cmd1(0), cmd2(0), ls1(0), ls2(0), ls3(0) {}
+            bool operator == (const NotificationEntry& other)const;
         };
+        void NakCurrentMessage(const std::vector<unsigned char>& curMessage, int& delayQueue, std::deque<unsigned char>& leftOver, std::shared_ptr<InsteonCommand> p);
         mutable std::mutex    m_mutex;
         std::ofstream   m_errorFile;
         std::unique_ptr<PlmMonitorIO> m_io;
@@ -159,8 +171,8 @@ namespace w5xdInsteon {
         std::deque<InsteonDevicePtr>  m_retiredDevices;
         std::thread m_thread;
 
-		// X10 devices
-		std::shared_ptr<X10DimmerCollection> m_X10Dimmers;
+        // X10 devices
+        std::shared_ptr<X10DimmerCollection> m_X10Dimmers;
 
         bool m_haveAllModemLinks;
         typedef std::vector<InsteonLinkEntry> LinkTable_t;
@@ -176,11 +188,12 @@ namespace w5xdInsteon {
         void syncWithThreadState(bool);
         void signalThreadStartStop(bool);
         int MessageGetImInfo();
-        void timeStamp(std::ostream &st);
+        void timeStamp(std::ostream& st);
         void deliverfromRemoteMessage(std::shared_ptr<std::vector<unsigned char> >, std::shared_ptr<InsteonCommand>);
-        void reportErrorState(const unsigned char *, int, std::shared_ptr<InsteonCommand>);
-        void getNextLinkRecordCompleted(InsteonCommand *);
-        int getDeviceLinkGroup(InsteonDevice *d)const;
+        void reportErrorState(const unsigned char*, int, std::shared_ptr<InsteonCommand>);
+        void getNextLinkRecordCompleted(InsteonCommand*);
+        void getLinkRecordNak(InsteonCommand*, const std::vector<unsigned char>&);
+        int getDeviceLinkGroup(InsteonDevice* d)const;
         void forceGetImInfo();
         void writeCommand(std::shared_ptr<InsteonCommand>);
     };
